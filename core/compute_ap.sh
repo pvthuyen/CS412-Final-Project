50670c3f81ba5856c61d366ad23fd3a9287b4bcd
Thuyen Phan <pvthuyen@apcs.vn>
2017-01-10 15:59:21 +0700

    delete unnecessary files for web deploy.

--- core/compute_ap.sh
+++
@@ -1,8 +0,0 @@
-g++ utils/compute_ap/compute_ap.cpp -o utils/compute_ap/compute_ap
-
-for file in data/rankedlists/*.txt; do
-    filename=${file##*/}
-    basename=${filename%_query.txt}
-    echo $basename
-    utils/compute_ap/compute_ap "data/groundtruths/$basename" "data/rankedlists/${basename}_query.txt" > "data/ap/${basename}_query.txt"
-done
